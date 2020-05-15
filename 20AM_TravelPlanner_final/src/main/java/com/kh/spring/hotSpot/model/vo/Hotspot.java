3edfac216e0036d92b17d4a84633793683944efa
2eegm <60306586+kmkevin@users.noreply.github.com>
2020-05-15 09:34:12 +0900

    #02 Hotspotvo 추가 -규민

---
+++ 20AM_TravelPlanner_final/src/main/java/com/kh/spring/hotSpot/model/vo/Hotspot.java
@@ -0,0 +1,18 @@
+package com.kh.spring.hotSpot.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hotspot {
+
+	private String hotspotName;
+	private String hotspotAreaName;
+	private String hotspotAddr;
+	private String hotspotContent;
+	private String hotspotImage;
+	private int hotspotView;
+}
