f4634fadc3d8593eed5dfb3e88d6d47e8a058269
82107 <82107@DESKTOP-A2P1E6D>
2020-05-22 10:37:53 +0900

    #15-2 페이징import 변경 -용진-

--- 20AM_TravelPlanner_final/src/main/java/com/kh/spring/hotSpot/controller/HotSpotController.java
+++ 20AM_TravelPlanner_final/src/main/java/com/kh/spring/hotSpot/controller/HotSpotController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.kh.spring.common.HotSpotSearchPaging;
 import com.kh.spring.hotSpot.model.service.HotSpotService;
 import com.kh.spring.hotSpot.model.vo.HotSpot;
 import com.kh.spring.member.model.service.MemberService;
